470caa48362997fe7e36e9a5b5f9c620ac778d1c
Jaime Rodriguez <jaimerodriguez@gmail.com>
2023-09-14 18:32:14 -0500

    First version

---
+++ formatter/whatsapp_formatter.py
@@ -0,0 +1,20 @@
+
+class FormatCommand:
+    def __init__(self, text) -> None:
+        self.text = text
+    
+    def execute(self):
+        raise Exception("Pls implement this method.")
+    
+class BoldCommand:
+    '''TODO Implement'''
+    pass
+
+class CursiveCommand:
+    '''TODO Implement'''
+    pass
+
+class MonospaceCommand:
+    '''TODO Implement'''
+    pass
+
